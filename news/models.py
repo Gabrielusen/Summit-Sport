446f901c7a2ff85f7c4edfd79046b5cc1f537881
Gabriel ufot <gabrielufot23@gmail.com>
2021-01-08 17:27:10 +0100

    headline model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    link = models.URLField(max_length=250, null=True, blank=True)
+    image = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
