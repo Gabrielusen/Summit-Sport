8132db47093fdea76d52a12b6af22bf6c8fc42e2
Gabriel ufot <gabrielufot23@gmail.com>
2021-01-27 13:36:56 +0100

    base.html

--- news/views.py
+++ news/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView
+from .models import News
 
 
-def index(request):
-    return HttpResponse('Hello World!')
+class IndexView(ListView):
+    queryset = News.objects.all()
+    template_name = 'base.html'
